056e9eda11c7e0d4bb26283ff247b293bca4e09b
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-11-11 23:49:57 +0330

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
+import styled from "styled-components";
+
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact />
-        <Route path="/Store" />
-        <Route path="/About" />
-        <Route path="/Services" />
-      </Routes>
-    </Router>
+    <Container>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+          <Route path="/Store" />
+          <Route path="/About" />
+          <Route path="/Services" />
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
 export default App;
+const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  background: linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%);
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,62 +1,140 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Header>
-      <nav>
-        <Div>
-          <Link to="/" className="nav-logo">
-            Cosmetic <i className="fas fa-spa"></i>
-          </Link>
-        </Div>
-        <Ulnav>
-          <Link to="/">
-            <Li>Home</Li>
-          </Link>
-          <Link to="/Store">
-            <Li>Store</Li>
-          </Link>
-          <Link to="/About">
-            <Li>About Us</Li>
-          </Link>
-          <Link to="/Services">
-            <Li>Services</Li>
-          </Link>
-        </Ulnav>
-      </nav>
-    </Header>
+    <header>
+      <Nav>
+        <NavLink
+          to="/"
+          className="nav_link"
+          style={{
+            fontWeight: "bold",
+
+            fontSize: "2rem",
+            color: "#fff",
+            textDecoration: "none",
+          }}
+        >
+          Cosmetic <Icon className="fas fa-spa"></Icon>
+        </NavLink>
+        <Hamberger onClick={() => setIsOpen(!isOpen)}>
+          <span />
+          <span />
+          <span />
+        </Hamberger>
+        <MenuUl isOpen={isOpen}>
+          <MenuLi>
+            <NavLink
+              to="/"
+              className="nav_link"
+              style={{
+                display: "inline",
+                color: "#fff",
+                textDecoration: "none",
+              }}
+            >
+              Home
+            </NavLink>
+          </MenuLi>
+          <MenuLi>
+            <NavLink
+              to="/Store"
+              className="nav_link"
+              style={{
+                display: "inline",
+                color: "#fff",
+                textDecoration: "none",
+              }}
+            >
+              Store
+            </NavLink>
+          </MenuLi>
+          <MenuLi>
+            <NavLink
+              to="/Services"
+              className="nav_link"
+              style={{
+                display: "inline",
+                color: "#fff",
+                textDecoration: "none",
+              }}
+            >
+              Services
+            </NavLink>
+          </MenuLi>
+          <MenuLi>
+            <NavLink
+              to="/About"
+              className="nav_link"
+              style={{
+                display: "inline",
+                color: "#fff",
+                textDecoration: "none",
+              }}
+            >
+              About Us
+            </NavLink>
+          </MenuLi>
+        </MenuUl>
+      </Nav>
+    </header>
   );
 }
+const Nav = styled.nav`
+  background-color: #201e1e;
+  color: #fff;
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+const Hamberger = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  span {
+    height: 2px;
+    width: 25px;
+    background: #b46868;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+const MenuUl = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  @media (max-width: 768px) {
+    overflow: hidden;
+    flex-direction: column;
+    width: 100%;
+    max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
+    transition: max-heght 0.3s ease-in;
+  }
+`;
+const MenuLi = styled.li`
+  display: inline;
+  padding: 1rem 2rem;
+  transition: all 0.2s ease-in;
+  &::hover {
+    color: #b46868;
+    font-weight: 700;
+    font-size: large;
+  }
+`;
+const Icon = styled.i`
+  color: #b46868;
+  font-weight: 900;
+`;
 
 export default Navbar;
-const Header = styled.header`
-  background-color: #181717;
-  font-size: 24px;
-  color: #fff;
-
-  width: 100%;
-  height: 80px;
-`;
-const Div = styled.div`
-  justify-content: start;
-  padding-top: 20px;
-  margin: 5px;
-  color: #fff;
-`;
-const Ulnav = styled.ul`
-  display: flex;
-  justify-content: end;
-  align-items: flex-end;
-  list-style: none;
-`;
-const Li = styled.li`
-  padding-left: 20px;
-  padding-bottom: 50px;
-`;
-
-const Button = styled.button`
-  border: 1px solid #fff;
-  width: 150px;
-  height: 150px;
-`;
