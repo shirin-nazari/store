f9befe6b7dd37e43b92bafe848f195ef64ac021e
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-11-18 11:56:45 +0330

    add article

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,68 @@
 import React from "react";
+import styled from "styled-components";
 import Card from "./Card";
 
 function Cards() {
   return (
-    <article>
-      <Card />
-    </article>
+    <>
+      <ArticleCard>
+        <Card
+          src="https://www.sephora.com/productimages/sku/s2419711-main-zoom.jpg?imwidth=315"
+          name="lip stick"
+          price="$21.00"
+          text=" A stacked deck of five holiday-exclusive Matte, Cremesheen and Frost
+        shades."
+        />
+        <Card
+          src="https://www.sephora.com/productimages/sku/s2419711-main-zoom.jpg?imwidth=315"
+          name="lip stick"
+          price="$21.00"
+          text=" A stacked deck of five holiday-exclusive Matte, Cremesheen and Frost
+        shades."
+        />
+        <Card
+          src="https://www.sephora.com/productimages/sku/s2419711-main-zoom.jpg?imwidth=315"
+          name="lip stick"
+          price="$21.00"
+          text=" A stacked deck of five holiday-exclusive Matte, Cremesheen and Frost
+        shades."
+        />
+      </ArticleCard>
+      <ArticleCard>
+        <Card
+          src="https://www.sephora.com/productimages/sku/s2419711-main-zoom.jpg?imwidth=315"
+          name="lip stick"
+          price="$21.00"
+          text=" A stacked deck of five holiday-exclusive Matte, Cremesheen and Frost
+        shades."
+        />
+        <Card
+          src="https://www.sephora.com/productimages/sku/s2419711-main-zoom.jpg?imwidth=315"
+          name="lip stick"
+          price="$21.00"
+          text=" A stacked deck of five holiday-exclusive Matte, Cremesheen and Frost
+        shades."
+        />
+        <Card
+          src="https://www.sephora.com/productimages/sku/s2419711-main-zoom.jpg?imwidth=315"
+          name="lip stick"
+          price="$21.00"
+          text=" A stacked deck of five holiday-exclusive Matte, Cremesheen and Frost
+        shades."
+        />
+      </ArticleCard>
+    </>
   );
 }
-
+const ArticleCard = styled.article`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
 export default Cards;
