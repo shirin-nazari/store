f843b2351b762ba6b4d3db683692ae1de5400a52
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-12-11 16:40:32 +0330

    add section

---
+++ src/components/SectionFooter.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+
+function SectionFooter() {
+  return (
+    <div>
+      <article>
+        <span>
+          <h2>Kind To Skin & Planet</h2>
+          We've been making products that respect your skin and the planet for a
+          full-circle clean—from source to disposal—since 1984.
+        </span>
+        {/* <img src="./images/kisspng-beauty-parlour-cosmetics-food-samosa-5af4fdf389fc62.png" /> */}
+      </article>
+    </div>
+  );
+}
+
+export default SectionFooter;
