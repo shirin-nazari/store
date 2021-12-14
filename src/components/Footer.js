e46a35a783aa661303a21c553348d42b6240d4b6
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-12-14 21:50:15 +0330

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
 function Footer() {
   return (
-    <sidebar>
-      this is footer
-      <div>contect us width me</div>
+    <FooterMa>
+      <FooterHeader>
+        <hr></hr>
+        <p>Markups </p>
+        <hr></hr>
+      </FooterHeader>
+      <div>contect us</div>
+      <div></div>
       <div>
         <i className="fab fa-facebook-square"></i>
         <i className="fab fa-twitter"></i>
@@ -12,8 +18,30 @@ function Footer() {
         <i className="fab fa-telegram-plane"></i>
         <i className="fab fa-linkedin-in"></i>
       </div>
-    </sidebar>
+    </FooterMa>
   );
 }
 
+const FooterMa = styled.div`
+  /* display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between; */
+  padding: 20px;
+`;
+const FooterHeader = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  hr {
+    width: 30%;
+    height: 0px;
+    margin-top: 15px;
+    color: #fff;
+    background: #fff;
+  }
+  p {
+    font-family: "Luxurious Script", cursive;
+    font-size: 30px;
+  }
+`;
 export default Footer;
