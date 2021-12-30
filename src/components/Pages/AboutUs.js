515c14981ac96fd5e43dd3ef80aa65377ac000f8
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-12-30 23:27:08 +0330

    fix back

--- src/components/Pages/AboutUs.js
+++ src/components/Pages/AboutUs.js
@@ -1,13 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import Background from "../images/rcfgoijziiqaxwnp05dnpgiesnbsghxkmt3xs4r2gfqpk3imvowfpbpwf3nfgwnh-.jpg";
+import Background from "../images/fashion-accessories-makeup-products-jewelry-handbag-pastel-background-beauty-fashion-concept-flat-lay_151851-277.jpg";
 
 function AboutUs() {
-  return <MainDiv>gfjaklskjsaksks</MainDiv>;
+  return (
+    <MainDiv style={{ backgroundImage: "url(" + Background + ")" }}>
+      gfjaklskjsaksks
+    </MainDiv>
+  );
 }
 const MainDiv = styled.div`
-  background-image: url(Background);
+  /* background-image: url(Background); */
   background-position: center;
+  background-size: cover;
   background-repeat: no-repeat;
   width: 100%;
   height: 100%;
