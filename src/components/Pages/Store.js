9f4448f2afc31b3411184ace160b03bdeb4e7b1e
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-01-06 17:16:04 +0330

    add store

--- src/components/Pages/Store.js
+++ src/components/Pages/Store.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import Cards from "../Cards";
+import CardMains from "../CardMains";
+import styled from "styled-components";
 function Store() {
-  return <div> ggggggggg</div>;
+  return (
+    <Div>
+      <Cards />
+      <CardMains />
+    </Div>
+  );
 }
-
+const Div = styled.div`
+  /* margin: 20px;
+  width: 100%;
+  height: 100%; */
+  padding: 20px;
+`;
 export default Store;
